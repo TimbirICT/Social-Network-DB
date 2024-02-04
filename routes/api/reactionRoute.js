8334287ae1c1a2d50f5ac480b912b7b6ab2adc32
Timbir <tmiddlebrooks99@gmail.com>
2024-02-03 22:27:38 -0600

    fixed api index

--- routes/api/reactionRoute.js
+++ routes/api/reactionRoute.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const {
   createReaction,
-  deleteReaction,
+  removeReaction, // Make sure this method is correctly referenced
 } = require('../../controllers/reactionController');
 
 // /api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions')
-  .post(createReaction)
-  .delete(deleteReaction);
+  .post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction); // Ensure correct method reference
 
 module.exports = router;
